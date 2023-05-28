3d2d79381ca5f91d493163e43a9fca45c81b5f1b
Shivam77607 <shivamsharma77607@gmail.com>
2023-05-28 09:48:55 +0530

    feat: add task CRUD route

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,7 +1,27 @@
 function getAllTasks(req, res) {
-	res.send("All items");
+	res.send("get all task");
+}
+
+function createTask(req, res) {
+	res.send("create task");
+}
+
+function getTask(req, res) {
+	res.send("get a task");
+}
+
+function updateTask(req, res) {
+	res.send("update a task");
+}
+
+function deleteTask(req, res) {
+	res.send("delete a task");
 }
 
 module.exports = {
-	getAllTasks
+	getAllTasks,
+	createTask,
+	getTask,
+	updateTask,
+	deleteTask
 };
