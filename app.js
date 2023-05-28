be1085fd12af3a4666f92f3a30a5c03a355afa96
Shivam77607 <shivamsharma77607@gmail.com>
2023-05-28 19:31:11 +0530

    feat: setup mongo server connection

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
+const connectDB = require("./db/connect");
 const express = require("express");
 const app = express();
 const tasks = require("./routes/tasks");
+require("dotenv").config();
 
 //middleware
 app.use(express.json());
@@ -13,4 +15,14 @@ app.get("/", (req, res) => {
 app.use("/api/v1/tasks", tasks);
 
 const port = 3000;
-app.listen(port);
+
+async function start() {
+	try {
+		await connectDB(process.env.MONGO_URI);
+		app.listen(port, console.log("server is running..."));
+	} catch (error) {
+		console.log(error);
+	}
+}
+
+start();
