3f2f5217d9ab87a4c0f36c8b492c9c2b0e045f5f
Shivam77607 <shivamsharma77607@gmail.com>
2023-05-28 09:30:38 +0530

    feat: implement basic root route

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log("Task Manager App");
+const express = require("express");
+const app = express();
+
+const routes = require("./routes/tasks");
+
+app.get("/", (req, res) => {
+	res.send("Task Manager");
+});
+
+const port = 3000;
+app.listen(port);
