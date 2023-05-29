4ceb1547c828c15168ab6e682cf9282503425689
Shivam77607 <shivamsharma77607@gmail.com>
2023-05-29 12:40:28 +0530

    feat: add error handler

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const app = express();
 const tasks = require("./routes/tasks");
 const notFound = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
 require("dotenv").config();
 
 //middleware
@@ -12,6 +13,7 @@ app.use(express.json());
 //routes
 app.use("/api/v1/tasks", tasks);
 app.use(notFound);
+app.use(errorHandlerMiddleware);
 
 const port = 3000;
 
---
+++ middleware/error-handler.js
@@ -0,0 +1,5 @@
+function errorHandlerMiddleware(err, req, res, next) {
+	return res.status(500).json({ msg: err });
+}
+
+module.exports = errorHandlerMiddleware;
