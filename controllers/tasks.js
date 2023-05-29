e36435c04c13201e1d8776ffffff2c1de777b2c3
Shivam77607 <shivamsharma77607@gmail.com>
2023-05-29 11:15:15 +0530

    feat: add api router functionality

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,24 +1,60 @@
 const Task = require("../models/Task");
 
-function getAllTasks(req, res) {
-	res.send("get all task");
+async function getAllTasks(req, res) {
+	try {
+		const tasks = await Task.find({});
+		res.status(200).json({ tasks });
+	} catch (error) {
+		res.status(500).json({ msg: error });
+	}
 }
 
 async function createTask(req, res) {
-	const task = await Task.create(req.body);
-	res.status(201).json({ task });
+	try {
+		const task = await Task.create(req.body);
+		res.status(201).json({ task });
+	} catch (error) {
+		res.status(500).json({ msg: error });
+	}
 }
 
-function getTask(req, res) {
-	res.json({ id: req.params.id });
+async function getTask(req, res) {
+	try {
+		const { id: taskId } = req.params;
+		const task = await Task.findOne({ _id: taskId });
+		if (!task)
+			return res.status(404).json({ msg: `No task with id: ${taskId}` });
+		res.status(200).json({ task });
+	} catch (error) {
+		res.status(500).json({ msg: error });
+	}
 }
 
-function updateTask(req, res) {
-	res.send(req.body);
+async function updateTask(req, res) {
+	try {
+		const { id: taskId } = req.params;
+		const task = await Task.findOneAndUpdate({ _id: taskId }, req.body, {
+			new: true,
+			runValidators: true
+		});
+		if (!task)
+			return res.status(404).json({ msg: `No task with id: ${taskId}` });
+		res.status(200).json({ task });
+	} catch (error) {
+		res.status(500).json({ msg: error });
+	}
 }
 
-function deleteTask(req, res) {
-	res.send("delete a task");
+async function deleteTask(req, res) {
+	try {
+		const { id: taskId } = req.params;
+		const task = await Task.findOneAndDelete({ _id: taskId });
+		if (!task)
+			return res.status(404).json({ msg: `No task with id: ${taskId}` });
+		res.status(200).json({ task });
+	} catch (error) {
+		res.status(500).json({ msg: error });
+	}
 }
 
 module.exports = {
