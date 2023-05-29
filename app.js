336ca0b8bad19691bc2aa53db290ad0c7d83921d
Shivam77607 <shivamsharma77607@gmail.com>
2023-05-29 13:18:40 +0530

    feat: add functionality to choose PORT

--- app.js
+++ app.js
@@ -15,12 +15,12 @@ app.use("/api/v1/tasks", tasks);
 app.use(notFound);
 app.use(errorHandlerMiddleware);
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 async function start() {
 	try {
 		await connectDB(process.env.MONGO_URI);
-		app.listen(port, console.log("server is running..."));
+		app.listen(port, console.log(`Server is running on port ${port}`));
 	} catch (error) {
 		console.log(error);
 	}
